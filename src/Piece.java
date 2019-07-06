322a831c783c12bba32f1c164a3342ad7330c898
dchenmei <dchenmei@users.noreply.github.com>
2019-07-06 13:53:24 -0400

    Added piece color and changed access member variable access

--- src/Piece.java
+++ src/Piece.java
@@ -1,17 +1,35 @@
-public abstract class Piece {
-    private int posX;
-    private int posY;
+import java.awt.Color;
 
-    public Piece(int x, int y) {
+public abstract class Piece {
+    /* Public / private pattern instead of getters / setters */
+    public int posX;
+    public int posY;
+    public Color color;
+
+
+    public Piece(int x, int y, Color c) {
         posX = x;
         posY = y;
+        color = c;
     }
 
     public Piece() {
-        this(0, 0);
+        this(0, 0, Color.RED);
     }
 
-    public abstract void validMove(int x, int y);
+
+    /*
+        Only checks if the move is valid from the piece's standpoint, meaning
+        ignoring whether there's another piece in place or it goes out of the edge
+     */
+    public abstract boolean validMove(int x, int y);
+
+    public void move(int x, int y) {
+        if (validMove(x, y)) {
+            posX = x;
+            posY = y;
+        }
+    }
 
     public String toString() {
         return "Piece at " + posX + ", " + posY;
